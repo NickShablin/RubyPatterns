35e12e61bd7e477ca27d88d220c3cabc64032b01
NickShablin <sl1pp3r15@yandex.ru>
2024-11-30 13:37:09 +0300

    Lab №1 [Zadanie №4] work with numbers
    Переопределение функции проверки числа на простоту

--- vvedenie/numbers.rb
+++ vvedenie/numbers.rb
@@ -1,19 +1,18 @@
-# Метод 1: Найти сумму непростых делителей числа
-def sum_non_prime_divisors(n)
-  def prime?(num)
-    return false if num <= 1
-    (2..Math.sqrt(num).to_i).none? { |i| num % i == 0 }
-  end
-
-  (1..n).select { |i| n % i == 0 && !prime?(i) }.sum
+# Функция для проверки, является ли число простым
+def prime?(num)
+  return false if num <= 1
+  (2..Math.sqrt(num).to_i).none? { |i| num % i == 0 }
 end
 
+# Метод 1: Найти сумму непростых делителей числа
+def sum_non_prime_divisors(n)
+  (1..n).select { |i| n % i == 0 && !prime?(i) }.sum
+end
 
 # Метод 2: Найти количество цифр числа, меньших 3
 def count_digits_less_than_3(n)
   n.to_s.chars.count { |digit| digit.to_i < 3 }
 end
-
 
 # Метод 3: Найти количество чисел, не являющихся делителями исходного числа,
 # не взаимно простых с ним и взаимно простых с суммой простых цифр числа
@@ -22,8 +21,7 @@ def count_special_numbers(n)
     b == 0 ? a : gcd(b, a % b)
   end
 
-  prime_digits = [2, 3, 5, 7]
-  sum_prime_digits = n.to_s.chars.map(&:to_i).select { |digit| prime_digits.include?(digit) }.sum
+  sum_prime_digits = n.to_s.chars.map(&:to_i).select { |digit| prime?(digit) }.sum
 
   (1...n).reject { |x| n % x == 0 }
          .select { |x| gcd(x, n) != 1 && gcd(x, sum_prime_digits) == 1 }
@@ -33,5 +31,4 @@ end
 n = 12
 puts "Метод 1: Сумма непростых делителей числа #{n}: #{sum_non_prime_divisors(n)}"
 puts "Метод 2: Количество цифр числа #{n}, меньших 3: #{count_digits_less_than_3(n)}"
-puts "Метод 3: Количество специальных чисел для, не являющихся делителями исходного числа,
-	не взаимно простых с ним и взаимно простых с суммой простых цифр числа #{n}: #{count_special_numbers(n)}"
+puts "Метод 3: Количество специальных чисел для, не являющихся делителями исходного числа, не взаимно простых с ним и взаимно простых с суммой простых цифр числа #{n}: #{count_special_numbers(n)}"
