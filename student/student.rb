9a6be9a6fb16c6aece3be962b895441664af1ccb
NickShablin <sl1pp3r15@yandex.ru>
2024-12-28 13:40:44 +0300

    Lab №2 Part1 [Zadanie №8] atributes validation
    Создать валидации для корректной формы строки в
    остальных полях.

--- student/student.rb
+++ student/student.rb
@@ -5,13 +5,13 @@ class Student
   # Конструктор, принимающий хэш
   def initialize(attributes = {})
     @id = attributes[:id]
-    @surname = attributes[:surname]
-    @name = attributes[:name]
-    @patronymic = attributes[:patronymic]
+    self.surname = attributes[:surname]
+    self.name = attributes[:name]
+    self.patronymic = attributes[:patronymic]
     self.phone = attributes[:phone]  # Используем сеттер для проверки
-    @telegram = attributes[:telegram]
-    @email = attributes[:email]
-    @git = attributes[:git]
+    self.telegram = attributes[:telegram]
+    self.email = attributes[:email]
+    self.git = attributes[:git]
   end
 
   # Метод класса для проверки корректности телефонного номера
@@ -19,10 +19,60 @@ class Student
     phone.match?(/\A(\+\d{1,3}\s?)?\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}\z/)
   end
 
-  # Сеттер для phone с проверкой
+  # Метод класса для проверки корректности фамилии, имени и отчества
+  def self.validate_name(name)
+    name.match?(/\A[а-яА-ЯёЁa-zA-Z\s-]+\z/)
+  end
+
+  # Метод класса для проверки корректности email
+  def self.validate_email(email)
+    email.match?(/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i)
+  end
+
+  # Метод класса для проверки корректности Telegram
+  def self.validate_telegram(telegram)
+    telegram.match?(/\A@\w+\z/)
+  end
+
+  # Метод класса для проверки корректности Git
+  def self.validate_git(git)
+    git.match?(/\Ahttps?:\/\/github\.com\/[\w\-]+\z/)
+  end
+
+  # Сеттеры с проверками
+  def surname=(value)
+    raise ArgumentError, "Недопустимая фамилия" unless self.class.validate_name(value)
+    @surname = value
+  end
+
+  def name=(value)
+    raise ArgumentError, "Недопустимое имя" unless self.class.validate_name(value)
+    @name = value
+  end
+
+  def patronymic=(value)
+    raise ArgumentError, "Недопустимое отчество" unless self.class.validate_name(value)
+    @patronymic = value
+  end
+
   def phone=(value)
     raise ArgumentError, "Недопустимый номер телефона" unless self.class.validate_phone(value)
     @phone = value
+  end
+
+  def telegram=(value)
+    raise ArgumentError, "Недопустимый Telegram" unless self.class.validate_telegram(value)
+    @telegram = value
+  end
+
+  def email=(value)
+    raise ArgumentError, "Недопустимый email" unless self.class.validate_email(value)
+    @email = value
+  end
+
+  def git=(value)
+    raise ArgumentError, "Недопустимый Git" unless self.class.validate_git(value)
+    @git = value
   end
 
   # Метод для вывода информации о студенте
