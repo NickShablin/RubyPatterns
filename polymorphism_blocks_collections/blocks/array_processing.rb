7744d12c6ccd0aedaaf871a8eb5d7406632b953c
NickShablin <sl1pp3r15@yandex.ru>
2025-02-15 11:13:57 +0300

    Lab №3 Part2 [Zadanie №2] find_all method
    Написать свою реализацию для метода find_all

--- polymorphism_blocks_collections/blocks/array_processing.rb
+++ polymorphism_blocks_collections/blocks/array_processing.rb
@@ -17,4 +17,14 @@ class ArrayProcessor
     end
     false
   end
+
+    # find_all – возвращает массив всех элементов, для которых блок возвращает true
+  def find_all
+    raise LocalJumpError, "не передан блок" unless block_given?
+    result = []
+    for elem in @array
+      result << elem if yield(elem)
+    end
+    result
+  end
 end
--- polymorphism_blocks_collections/blocks/tests.rb
+++ polymorphism_blocks_collections/blocks/tests.rb
@@ -1,27 +1,41 @@
 require_relative 'array_processing'
 
-
+# Функция для проверки равенства и вывода сообщения
 def assert_equal(expected, actual, message = "")
   if expected == actual
     puts "Тест пройден: #{message}"
   else
-    raise "Тест не пройден: #{message}\n  Ожидаемый результат: #{expected.inspect}\n Фактический результат:   #{actual.inspect}"
+    raise "Тест не пройден: #{message}\n  Ожидаемый результат: #{expected.inspect}\n  Фактический результат: #{actual.inspect}"
   end
+end
+
+# Тестирование метода any?
+def test_any
+  processor = ArrayProcessor.new([1, 2, 3, 4, 5])
+  
+  result = processor.any? { |n| n > 3 }
+  assert_equal(true, result, "any? возвращает true, если найден элемент больше 3")
+  
+  result = processor.any? { |n| n > 5 }
+  assert_equal(false, result, "any? возвращает false, если нет элемента больше 5")
+end
+
+# Тестирование метода find_all
+def test_find_all
+  processor = ArrayProcessor.new([1, 2, 3, 4, 5])
+  
+  result = processor.find_all { |n| n.even? }
+  assert_equal([2, 4], result, "find_all возвращает массив четных чисел для [1,2,3,4,5]")
+  
+  result = processor.find_all { |n| n > 5 }
+  assert_equal([], result, "find_all возвращает пустой массив, если нет элементов больше 5")
 end
 
 def run_tests
   puts "Запуск тестов для ArrayProcessor..."
-
-  arr = [1, 2, 3, 4, 5]
-  processor = ArrayProcessor.new(arr)
-
-  # Тест метода any?
-  result = processor.any? { |n| n > 3 }
-  assert_equal(true, result, "any? возвращает true, если найден элемент больше 3")
-
-  result = processor.any? { |n| n > 5 }
-  assert_equal(false, result, "any? возвращает false, если нет элемента больше 5")
-
+  test_any
+  test_find_all
+  
 end
 
 run_tests
