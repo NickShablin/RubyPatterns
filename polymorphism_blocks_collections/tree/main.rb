cf806c4faed9a1b7814eea2d39b7bfac8ad32282
NickShablin <sl1pp3r15@yandex.ru>
2025-02-15 12:03:34 +0300

    Lab №3 Part3 [Zadanie №1] html tree
    Задание 3 Написать класс — дерево html, принимающий как аргумент
    произвольный html и сохраняющий теги с сохранением иерархии. Для
    упрощения можно использовать ограниченный набор тегов и их
    параметров. Создать класс тег с несколькими методами, возвращающими
    строку, число, булеан и хранить в дереве экземпляры класса тег.
    Подмешать Enumerable в дерево двумя способами — в длину и в
    ширину. Написание html

---
+++ polymorphism_blocks_collections/tree/main.rb
@@ -0,0 +1,10 @@
+html = "
+<html>
+  <body>
+    <div class=\"container\">
+      <p>Hello, World!</p>
+      <span>Sample Text</span>
+    </div>
+    <footer>Footer content</footer>
+  </body>
+</html>"
